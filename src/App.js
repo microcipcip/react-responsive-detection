de7f4a9481ef72fe9bf81734d8365a0c292018ea
Salvatore Tedde <microcipcip@gmail.com>
2020-01-15 15:55:10 +0000

    feat: remove unused props

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <ResponsiveContext.Provider value={responsiveData}>
       <div className="App">
         <header className="App-header">
-          <Table responsiveData={responsiveData} />
+          <Table />
         </header>
       </div>
     </ResponsiveContext.Provider>
